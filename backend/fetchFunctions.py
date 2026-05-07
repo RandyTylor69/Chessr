7d081b12606946adccd70171d17beedd7ef9c63b
ziyin <ziyinmao2006@gmail.com>
2026-05-06 20:08:11 -0400

    ready to create SQLite db

---
+++ backend/fetchFunctions.py
@@ -0,0 +1,36 @@
+import requests, json, os
+from dotenv import find_dotenv, load_dotenv
+
+# load env variables from root
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+API_TOKEN = os.getenv("API_TOKEN")
+username = os.getenv("username")
+
+# function lists
+def fetchGamesFromUser(username, token, amount):
+    try:
+        res = requests.get(
+            f"https://lichess.org/api/games/user/{username}",
+
+            headers={
+                "Accept":"application/x-ndjson",
+                "Authorization": f"Bearer {token}"
+            },
+            params={
+                "max":amount
+            }
+        )
+        result = []
+        for line in res.iter_lines():
+            data = json.loads(line)
+            result.append(data)
+        
+        return result
+
+    except Exception as e:
+        return "something's off"
+
+data = fetchGamesFromUser("SuperHeavyBattleship", API_TOKEN, 1)
+
+print(data)
